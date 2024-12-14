08890a79617b31712151c7cb45c4d15bfda0fcd8
JANNIES <janniesvarghese@gmail.com>
2024-12-14 21:06:47 +0530

    first

--- lib/Service/Database_Service.dart
+++ lib/Service/Database_Service.dart
@@ -1,4 +1,29 @@
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+
 class DatabaseService {
+  Database? _db;
   static final DatabaseService instance = DatabaseService._constructor();
   DatabaseService._constructor();
+
+  Future<Database> get database async {
+    if (_db != null) {
+      return _db!;
+    }
+    _db = await getDataBase();
+    return _db!;
+  }
+
+  Future<Database> getDataBase() async {
+    final databaseDirPath = await getDatabasesPath();
+    final databasePath = join(databaseDirPath, "Expense_db.db");
+    final database = await openDatabase(
+      databasePath,
+      onCreate: (db, version) {
+        db.execute(
+            'CREATE TABLE EXPENSE(ID TEXT PRIMARY KEY,TITLE TEXT,AMOUNT REAL,DATE TEXT,CATEGORY TEXT)');
+      },
+    );
+    return database;
+  }
 }
